91c37a4f33d45a85fb770e8e751e6e6027503a5a
INDIA TEAM <JapaEngagement@INDIAs-MacBook-Pro.local>
2018-12-12 14:16:44 +0530

    Updates

---
+++ precache-manifest.216f1151e664a844c6ce520a794987bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dfea15f5943074b63898",
+    "url": "/dead-simple-react-counter/static/js/runtime~main.dfea15f5.js"
+  },
+  {
+    "revision": "aa3709ab0714716d6633",
+    "url": "/dead-simple-react-counter/static/js/main.aa3709ab.chunk.js"
+  },
+  {
+    "revision": "a33ba73526a067a11661",
+    "url": "/dead-simple-react-counter/static/js/1.a33ba735.chunk.js"
+  },
+  {
+    "revision": "aa3709ab0714716d6633",
+    "url": "/dead-simple-react-counter/static/css/main.46461fa9.chunk.css"
+  },
+  {
+    "revision": "82da900707a871ad51e166d2ef744ac9",
+    "url": "/dead-simple-react-counter/index.html"
+  }
+];
